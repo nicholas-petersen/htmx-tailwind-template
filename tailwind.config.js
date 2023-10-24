e281b359be01204d62e14a9de492bbc8b9ed715b
np <np@air.local>
2023-10-24 15:32:28 +0200

    initial tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+	'./web/template/*.html',
+	'./web/template/views/*.html',
+	'./web/template/partials//*.html',
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
