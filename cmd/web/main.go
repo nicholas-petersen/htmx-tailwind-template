9971e2fc9cd5bd44a62eb8d85209ac59abadb5cb
np <np@air.local>
2023-10-24 16:24:18 +0200

    added webserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
 
+func main() {
+	port := 3001
+	staticFiles := http.FileServer(http.Dir("./web/static"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", staticFiles))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/wine", wine)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
+
+	err := srv.ListenAndServe()
+	log.Fatalf("Failed to ListenAndServe: %d", err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./web/template/base.html",
+		"./web/template/partials/nav.html",
+		"./web/template/views/home.html",
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func wine(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./web/template/base.html",
+		"./web/template/partials/nav.html",
+		"./web/template/views/wine.html",
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
